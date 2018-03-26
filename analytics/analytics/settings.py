7adff0b1ea57f9417cfe0cccd1c78deeb445ec08
W4pity <w4pity@gmail.com>
2018-03-26 23:14:02 +0200

    add staticfiles admin

--- analytics/analytics/settings.py
+++ analytics/analytics/settings.py
@@ -120,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATIC_ROOT = 'staticfiles'
+
+STATICFILES_DIRS = (os.path.join('static'), )
